2e9d70f9cd399c62ab1dc549f4f6752ed9f3e1d6
Wisdom <Wisdom@PUISSANCE>
2021-03-21 22:18:15 -0500

    Changes:
    Organization Profile Page, Individual Profile Page, Request page
    (frontend) and Request Servelt (Back End). Added Util files (Info &
    UtilDB). Only the Request Page is fully functional. I don't think I'm
    commiting my hibernate file. But let me know if you see it.

--- webproject/src/SubmitRequest.java
+++ webproject/src/SubmitRequest.java
@@ -1,39 +1,61 @@
 
+
 import java.io.IOException;
+import java.text.NumberFormat;
+import java.text.ParseException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import datamodel.*;
+import util.*;
+
 /**
- * Servlet implementation class Test
+ * Servlet implementation class SubmitRequest
  */
-@WebServlet("/Test")
-public class Test extends HttpServlet {
+@WebServlet("/SubmitRequest")
+public class SubmitRequest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Test() {
+    public SubmitRequest() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String id = request.getParameter("item");
+		try {
+			int item = ((Number)NumberFormat.getInstance().parse(id)).intValue();
+			
+			Posting post = new Posting();
+			post = UtilDB.FindListing(item);
+		
+			if(post == null) {
+//				notfound();
+			}
+			else {
+//				found();
+			}
+			
+		} 
+		catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
---
+++ webproject/src/util/UtilDB.java
@@ -0,0 +1,129 @@
+/**
+ */
+package util;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import datamodel.Role;
+import datamodel.*;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ * @since JavaSE-1.8
+ */
+public class UtilDB {
+   static SessionFactory sessionFactory = null;
+
+   public static SessionFactory getSessionFactory() {
+      if (sessionFactory != null) {
+         return sessionFactory;
+      }
+      Configuration configuration = new Configuration().configure();
+      StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+      sessionFactory = configuration.buildSessionFactory(builder.build());
+      return sessionFactory;
+   }
+
+   public static List<User> listUsers() {  //Retrieve Data
+      List<User> resultList = new ArrayList<User>();
+
+      Session session = getSessionFactory().openSession();
+      Transaction tx = null;
+
+      try {
+         tx = session.beginTransaction();
+         List<?> users = session.createQuery("FROM User").list(); //From NameofDataModel
+         for (Iterator<?> iterator = users.iterator(); iterator.hasNext();) {
+            User user = (User) iterator.next();
+            resultList.add(user);
+         }
+         tx.commit();
+      } catch (HibernateException e) {
+         if (tx != null)
+            tx.rollback();
+         e.printStackTrace();
+      } finally {
+         session.close();
+      }
+      return resultList;
+   }
+
+   public static List<User> listUsers(String email) { //Find User by Email
+      List<User> resultList = new ArrayList<User>();
+
+      Session session = getSessionFactory().openSession();
+      Transaction tx = null;
+
+      try {
+         tx = session.beginTransaction();
+         List<?> users = session.createQuery("FROM User").list();
+         for (Iterator<?> iterator = users.iterator(); iterator.hasNext();) {
+            User user = (User) iterator.next();
+            if (user.getEmail().startsWith(email)) {
+               resultList.add(user);
+            }
+         }
+         tx.commit();
+      } catch (HibernateException e) {
+         if (tx != null)
+            tx.rollback();
+         e.printStackTrace();
+      } finally {
+         session.close();
+      }
+      return resultList;
+   }
+   
+   public static Posting FindListing(int itemId) { //Find Posting by ID
+	      Posting result = new Posting();
+
+	      Session session = getSessionFactory().openSession();
+	      Transaction tx = null;
+
+	      try {
+	         tx = session.beginTransaction();
+	         List<?> posts = session.createQuery("FROM Posting").list();
+	         for (Iterator<?> iterator = posts.iterator(); iterator.hasNext();) {
+	            Posting post = (Posting) iterator.next();
+	            if (post.getId() == itemId) {
+	               result = post;
+	               break;
+	            }
+	         }
+	         tx.commit();
+	      } catch (HibernateException e) {
+	         if (tx != null)
+	            tx.rollback();
+	         e.printStackTrace();
+	      } finally {
+	         session.close();
+	      }
+	      return result;
+	   }
+
+   public static void createUsers(String firstName, String lastName, String email, 
+		   String password, Integer age, Role role) {  //Data Insertion
+      Session session = getSessionFactory().openSession();
+      Transaction tx = null;
+      try {
+         tx = session.beginTransaction();
+         session.save(new User(firstName, lastName, email, password, age, role));
+         tx.commit();
+      } catch (HibernateException e) {
+         if (tx != null)
+            tx.rollback();
+         e.printStackTrace();
+      } finally {
+         session.close();
+      }
+   }
+}
