7e8d60ac8868d8a756538d51bb40fa56b1046d14
kmpullen19 <kmpullen@unomaha.edu>
2021-04-20 21:41:01 -0500

    Fake commit.

--- webproject/src/ListingsIndex.java
+++ webproject/src/ListingsIndex.java
@@ -16,7 +16,7 @@ import com.google.gson.JsonObject;
 /**
  * Servlet implementation class Listings
  */
-@WebServlet("/Listings")
+@WebServlet("/ListingsIndex")
 public class ListingsIndex extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -31,31 +31,35 @@ public class ListingsIndex extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("application/json");
-		response.addHeader("Access-Control-Allow-Origin", getServletInfo());
-		PrintWriter out = response.getWriter();
 		
-		JsonObject listing1 = new JsonObject();
-		listing1.addProperty("id", "1");
-		listing1.addProperty("name", "Markers");
-		listing1.addProperty("description", "A bunch of Crayola markers. These are great markers. Really good. The best markers, some might say. People say, I don't say that, people say they're the best markers they've ever used");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 		
-		JsonObject listing2 = new JsonObject();
-		listing2.addProperty("id", "2");
-		listing2.addProperty("name", "Calculus Textbook");
-		listing2.addProperty("description", "Textbook from a class I don't need anymore.");
 		
-		JsonObject listing3 = new JsonObject();
-		listing3.addProperty("id", "3");
-		listing3.addProperty("name", "Notebook");
-		listing3.addProperty("description", "Black college-ruled notebook. Great condition.");
-		
-		JsonObject jsonResponse = new JsonObject();
-		jsonResponse.add("listing1", listing1);
-		jsonResponse.add("listing2", listing2);
-		jsonResponse.add("listing3", listing3);
-		
-		out.print(jsonResponse);
+//		response.setContentType("application/json");
+//		response.addHeader("Access-Control-Allow-Origin", getServletInfo());
+//		PrintWriter out = response.getWriter();
+//		
+//		JsonObject listing1 = new JsonObject();
+//		listing1.addProperty("id", "1");
+//		listing1.addProperty("name", "Markers");
+//		listing1.addProperty("description", "A bunch of Crayola markers. These are great markers. Really good. The best markers, some might say. People say, I don't say that, people say they're the best markers they've ever used");
+//		
+//		JsonObject listing2 = new JsonObject();
+//		listing2.addProperty("id", "2");
+//		listing2.addProperty("name", "Calculus Textbook");
+//		listing2.addProperty("description", "Textbook from a class I don't need anymore.");
+//		
+//		JsonObject listing3 = new JsonObject();
+//		listing3.addProperty("id", "3");
+//		listing3.addProperty("name", "Notebook");
+//		listing3.addProperty("description", "Black college-ruled notebook. Great condition.");
+//		
+//		JsonObject jsonResponse = new JsonObject();
+//		jsonResponse.add("listing1", listing1);
+//		jsonResponse.add("listing2", listing2);
+//		jsonResponse.add("listing3", listing3);
+//		
+//		out.print(jsonResponse);
 	}
 
 	/**
